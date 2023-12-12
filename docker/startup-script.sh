6f1e261c914f06f2986178ec88bcfd7224640a9b
p-rycu <piotrycek@gmail.com>
2023-12-12 19:22:49 +0100

    Editing the Docker runtime environment.

---
+++ docker/startup-script.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Wait until Cassandra is not ready
+until cqlsh -e "DESCRIBE KEYSPACES"; do
+    echo "CASSANDRA NOT READY - waiting 5 seconds"
+    sleep 5
+done
+
+# Import data
+cqlsh -f /cql-schema/create_schema.cql
+cqlsh -f /cql-schema/load_data.cql
+
+# Sleep infinity - container is alive
+sleep infinity
