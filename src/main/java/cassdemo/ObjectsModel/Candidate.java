b47e7b84326903014c7ee18f85227e504ecef751
Kacper Nadstoga <kapix007@gmail.com>
2023-12-14 11:46:20 +0100

    fill Candidate class with constructor

--- src/main/java/cassdemo/ObjectsModel/Candidate.java
+++ src/main/java/cassdemo/ObjectsModel/Candidate.java
@@ -2,6 +2,11 @@ public class Candidate {
     private String name;
     private String surname;
 
+    public Candidate(String _name, String _surname){
+        this.name = _name;
+        this.surname = _surname
+    }
+
     public String getName(){
         return this.name;
     }
